261b7e9cff2aadc68450017b05768c131f129677
Thomas Hass <t.hass@freenet.de>
2024-02-26 17:13:27 +0100

    - fix BestSound driver and preferences module

---
+++ Installed/Driver/Sound/BSD16/dependencies.mk
@@ -0,0 +1,12 @@
+manager.obj \
+manager.eobj: geos.def file.def geode.def dirk.def dirksnd.def \
+                resource.def ec.def driver.def lmem.def heap.def \
+                system.def localize.def sllang.def timer.def initfile.def \
+                char.def Internal/interrup.def bsConst.def bsError.def \
+                bsTimer.asm sound.def Internal/soundFmt.def \
+                Internal/semInt.def Internal/DMADrv.def \
+                Internal/strDrInt.def Internal/streamDr.def \
+                Internal/soundDrv.def bsError.asm bsRegs.asm bsInit.asm \
+                bsDelay.asm bsStrate.asm bsInt.asm bsVoice.asm bsDAC.asm \
+                bsStream.asm bsWav.asm bsMixer.asm mixLib.def \
+                bsRecord.asm bsNWav.asm
